c2a0465c56990cc14672adf50419010ab1851792
Xubili <guivpw68@gmail.com>
2020-11-15 04:02:45 -0300

    added api error handlers

---
+++ src/services/Error/index.ts
@@ -0,0 +1,56 @@
+const errorHandler = ({ status }) => {
+	let erro
+	switch (status) {
+		case 401:
+			erro = {
+				text: 'client_id incorreto.',
+				json: {
+					error:
+						'Could not find a required APP in the request, identified by HEADER client_id.'
+				}
+			}
+			return erro
+		case 403:
+			erro = {
+				text: 'Você não tem autorização para acessar essa API.',
+				json: {
+					error: 'The APP can not access this API'
+				}
+			}
+			return erro
+		case 504:
+			erro = {
+				text: 'Erro no servidor da Elo, contate o suporte!',
+				json: {
+					error: 'Gateway Time-out'
+				}
+			}
+			return erro
+		case 'keyPair':
+			erro = {
+				text: 'Erro na criação da keyPair, contate o suporte!',
+				json: {
+					error: 'Unknown Error'
+				}
+			}
+			return erro
+		case 'encrypt':
+			erro = {
+				text: 'Não foi possível criptografar o cartão! Analise seus dados!',
+				json: {
+					error: 'Encrypt Failed'
+				}
+			}
+			return erro
+		default:
+			erro = {
+				text: 'Erro desconhecido, contate o suporte!',
+				json: {
+					error: 'Unknown Error'
+				}
+			}
+			return erro
+	}
+}
+
+export default errorHandler
