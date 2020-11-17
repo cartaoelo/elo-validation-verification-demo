2b59ed6f66dad82c1989154ebc7f1597cced09c5
Xubili <guivpw68@gmail.com>
2020-11-17 10:51:03 -0300

    added validaCPF page styles

---
+++ src/styles/CPF/ValidateCPF.styled.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+const ValidateCPFContainerStyled = styled.div`
+	padding: 77px 30px 50px;
+	background-color: #fff;
+	border-radius: 5px;
+	width: 450px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	animation: ${({ out }: { out: boolean }) => out && '.5s ease-out 1 slideToTop'};
+
+	h1 {
+		font-size: 30px;
+		text-align: center;
+		margin-bottom: 55px;
+	}
+
+	@media only screen and (max-width: 1200px) {
+		width: auto;
+	}
+
+	@keyframes slideToTop {
+		0% {
+			opacity: 1;
+			transform: translateY(0);
+		}
+		100% {
+			opacity: 0;
+			transform: translateY(-10%);
+		}
+	}
+`
+
+export default ValidateCPFContainerStyled
