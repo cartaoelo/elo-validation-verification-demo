620267195d36626626a0b5ad26866deebf01e11e
Henry Kimura <htkimura@gmail.com>
2020-11-18 11:45:46 -0300

    added calendar styles

---
+++ src/components/Form/FormMonthYear/FormMonthYear.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const FormMonthYearStyled = styled.div`
+	display: flex;
+	div:last-child {
+		margin-left: 3%;
+	}
+`
+
+export default FormMonthYearStyled
