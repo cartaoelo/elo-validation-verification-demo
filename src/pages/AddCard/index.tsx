f159fdb9e7a0d619920da461c85ad1668bfcbf58
Henry Kimura <htkimura@gmail.com>
2020-11-18 11:46:24 -0300

    fixed header and add card form

--- src/pages/AddCard/index.tsx
+++ src/pages/AddCard/index.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form'
 import AddCardStyled from '../../components/AddCard/AddCard.styled'
 import FormButtonStyled from '../../components/Form/FormButton/FormButton.styled'
 import FormInput from '../../components/Form/FormInput'
+import FormMonthYearStyled from '../../components/Form/FormMonthYear/FormMonthYear.styled'
 import FormStyled from '../../styles/Home/LoginForm.styled'
 
 const AddCard = () => {
@@ -17,30 +18,30 @@ const AddCard = () => {
 
 	return (
 		<AddCardStyled>
-			<h1>Faça Login no Portal Elo</h1>
+			<h1>Adicionar cartão</h1>
 			<FormStyled onSubmit={onSubmit}>
 				<FormInput
-					boxIcons={{ name: 'envelope', type: 'solid' }}
+					boxIcons={{ name: 'credit-card', type: 'solid' }}
 					name="pan"
 					required
 					ref={register}
 				/>
-				<div className="">
+				<FormMonthYearStyled>
 					<FormInput
 						calendar
-						boxIcons={{ name: 'envelope', type: 'solid' }}
+						boxIcons={{ name: 'calendar', type: 'solid' }}
 						name="month"
 						required
 						ref={register}
 					/>
 					<FormInput
 						calendar
-						boxIcons={{ name: '', type: 'solid' }}
+						boxIcons={{ name: 'calendar-alt', type: 'solid' }}
 						name="year"
 						required
 						ref={register}
 					/>
-				</div>
+				</FormMonthYearStyled>
 				<FormInput
 					required
 					boxIcons={{ name: 'user', type: 'solid' }}
@@ -48,7 +49,7 @@ const AddCard = () => {
 					ref={register}
 				/>
 				<FormButtonStyled type="submit" disabled={stateCard.ended}>
-					Enviar
+					Adicionar
 				</FormButtonStyled>
 			</FormStyled>
 		</AddCardStyled>
