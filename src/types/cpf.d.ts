b0fde9d5b24bd293fe39aee651452d681cb06328
Xubili <guivpw68@gmail.com>
2020-11-18 22:19:46 -0300

    created cpf Score types

---
+++ src/types/cpf.d.ts
@@ -0,0 +1,11 @@
+export type Score = {
+	value: string
+	spendingIndex: 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h'
+	spendingIndex: string
+	digitalVarietyRisk: string
+	digitalBehaviourRisk: string
+	profileRisk: string
+	statusRisk: string
+	postalRisk: string
+	rapportRisk: string
+}
