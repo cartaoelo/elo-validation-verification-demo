84804690757596413da4b0eec273d554458327ce
Xubili <guivpw68@gmail.com>
2020-11-20 15:51:37 -0300

    added generate keypair & json

--- src/services/Sensitive/index.ts
+++ src/services/Sensitive/index.ts
@@ -1,26 +1,13 @@
-import jose from 'node-jose'
-
 import { jwsSign, jweEncrypt } from './jwtEncryptions'
+import key from './keypair.json'
 
-async function generateKeyPair({ kid }) {
-	const jwk = await jose.JWK.createKey('EC', 'P-256', { kid })
-	const key = {
-		pair: jwk.toJSON(true),
-		public: jwk.toJSON(false)
-	}
-	return { jwk, key }
-}
-
-export async function encryptCardData({ cardData, eloKey, kid }) {
-	const { jwk, key } = await generateKeyPair(kid)
+export async function encryptCardData({ cardData, eloKey }) {
+	const { pair } = key
 	const cardSensitiveDataStringified = JSON.stringify(cardData)
 
 	const serverKey = JSON.parse(eloKey)
-	serverKey.kid = kid
 
-	if (!jwk) console.log(jwk)
-
-	const signedJws = await jwsSign({ input: cardSensitiveDataStringified, key: jwk })
+	const signedJws = await jwsSign({ input: cardSensitiveDataStringified, key: pair })
 	const sensitive = await jweEncrypt({ input: signedJws, key: serverKey })
 	return { sensitive, key }
 }
