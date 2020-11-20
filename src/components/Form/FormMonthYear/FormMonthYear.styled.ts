67491f51b3e954f262e6760b457cd57ab7637c4e
Xubili <guivpw68@gmail.com>
2020-11-20 15:50:05 -0300

    changed FormExpiry responsive layout

--- src/components/Form/FormMonthYear/FormMonthYear.styled.ts
+++ src/components/Form/FormMonthYear/FormMonthYear.styled.ts
@@ -2,8 +2,16 @@ import styled from 'styled-components'
 
 const FormMonthYearStyled = styled.div`
 	display: flex;
+	width: 100%;
 	div:last-child {
 		margin-left: 3%;
+	}
+
+	@media only screen and (max-width: 600px) {
+		flex-direction: column;
+		div:last-child {
+			margin-left: 0;
+		}
 	}
 `
 
